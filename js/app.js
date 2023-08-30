8a07a8f79ce7e1659c1d9871d8838c0109665230
isradev-git <israelzamora1996@gmail.com>
2023-08-30 11:28:18 +0200

    Clip-Path pruebas y frames function

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ window.onscroll = function() {
 
 	frames.forEach(function(n, i) {
 		zVals.push((i * zSpacing) + zSpacing)
-		zVals[i] += delta * -5.5
+		zVals[i] += delta * -5 //5.5 original
 		let frame = frames[i],
 				transform = `translateZ(${zVals[i]}px)`,
 				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
